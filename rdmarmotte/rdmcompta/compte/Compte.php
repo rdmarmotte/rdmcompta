ed14be1339f617161c863926c6d78c65d9b7c4f5
Baptiste Pillot <baptiste.pillot@bappli.com>
2020-02-21 18:14:13 +0100

    Calculs des soldes de compte

--- rdmarmotte/rdmcompta/compte/Compte.php
+++ rdmarmotte/rdmcompta/compte/Compte.php
@@ -1,7 +1,10 @@
 <?php
 namespace RDMarmotte\RdmCompta;
 
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\Dao\Func;
 use ITRocks\Framework\Session;
+use ITRocks\Framework\Tools\Date_Time;
 use ITRocks\Framework\User;
 use RDMarmotte\RdmCompta\Compte\Menu;
 
@@ -12,7 +15,7 @@ use RDMarmotte\RdmCompta\Compte\Menu;
  * @after_write Menu::refresh
  * @business
  * @display_order libelle, titulaires, commentaire,
- *                solde_initial, solde_courant, solde_a_venir, solde_pointe, solde_rapproche
+ *                solde_initial, solde_rapproche, solde_pointe, solde_courant, solde_a_venir
  * @feature
  * @representative libelle
  * @see Menu
@@ -44,14 +47,18 @@ class Compte
 
 	//-------------------------------------------------------------------------------- $solde_a_venir
 	/**
+	 * @getter
 	 * @null
+	 * @user readonly
 	 * @var float
 	 */
 	public $solde_a_venir;
 
 	//-------------------------------------------------------------------------------- $solde_courant
 	/**
+	 * @getter
 	 * @null
+	 * @user readonly
 	 * @var float
 	 */
 	public $solde_courant;
@@ -59,20 +66,25 @@ class Compte
 	//-------------------------------------------------------------------------------- $solde_initial
 	/**
 	 * @null
+	 * @user create_only
 	 * @var float
 	 */
 	public $solde_initial;
 
 	//--------------------------------------------------------------------------------- $solde_pointe
 	/**
+	 * @getter
 	 * @null
+	 * @user readonly
 	 * @var float
 	 */
 	public $solde_pointe;
 
 	//------------------------------------------------------------------------------ $solde_rapproche
 	/**
+	 * @getter
 	 * @null
+	 * @user readonly
 	 * @var float
 	 */
 	public $solde_rapproche;
@@ -100,6 +112,76 @@ class Compte
 	public static function courant()
 	{
 		return Session::current()->get(static::class);
+	}
+
+	//-------------------------------------------------------------------------------- getSoldeAVenir
+	/**
+	 * @noinspection PhpUnused @getter
+	 * @return float
+	 */
+	protected function getSoldeAVenir()
+	{
+		return $this->solde_pointe + $this->operationsTotal(['pointage' => 'non']);
+	}
+
+	//------------------------------------------------------------------------------- getSoldeCourant
+	/**
+	 * @noinspection PhpUnused @getter
+	 * @return float
+	 */
+	protected function getSoldeCourant()
+	{
+		return $this->solde_pointe
+			+ $this->operationsTotal([
+				'pointage' => 'non',
+				'date'     => Func::lessOrEqual(Date_Time::now())
+			]);
+	}
+
+	//-------------------------------------------------------------------------------- getSoldePointe
+	/**
+	 * @noinspection PhpUnused @getter
+	 * @return float
+	 */
+	protected function getSoldePointe()
+	{
+		return $this->solde_rapproche + $this->operationsTotal(['pointage' => 'pointée']);
+	}
+
+	//----------------------------------------------------------------------------- getSoldeRapproche
+	/**
+	 * @noinspection PhpUnused @getter
+	 * @return float
+	 */
+	protected function getSoldeRapproche()
+	{
+		return $this->solde_initial + $this->operationsTotal(['pointage' => 'rapprochée']);
+	}
+
+	//------------------------------------------------------------------------------- operationsTotal
+	/**
+	 * @param $filtre array
+	 * @return float
+	 */
+	public function operationsTotal(array $filtre = [])
+	{
+		$total = 0;
+		$filtre['compte']    = $this;
+		$filtre['type.sens'] = 'émis';
+		Dao::select(
+			Operation::class,
+			['montant' => Func::sum()],
+			$filtre,
+			function(Operation $operation) use(&$total) { $total -= $operation->montant; }
+		);
+		$filtre['type.sens'] = 'reçu';
+		Dao::select(
+			Operation::class,
+			['montant' => Func::sum()],
+			$filtre,
+			function(Operation $operation) use(&$total) { $total += $operation->montant; }
+		);
+		return $total;
 	}
 
 }
